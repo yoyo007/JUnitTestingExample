1de6185c5fd2e3a8321e9e6637c6c5ba6ecb22b2
Derrek Bond <yoyo007@gmail.com>
2017-07-10 00:07:26 -0500

    Changed project from jar to war. Changed mockito to scope test. Added web xml and spring servlet xml for spring web. Added user controller to start first rest endpoint.

---
+++ src/main/java/com/yoyo007/junittestingexample/web/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.yoyo007.junittestingexample.web.controller;
+
+import com.yoyo007.junittestingexample.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+    @GetMapping
+    public ResponseEntity emptyUser(){
+        ResponseEntity<Object> response;
+        
+        User user = new User();
+//        user.setFirstName("first");
+//        user.setMiddileInitial("middle");
+//        user.setLastName("last");
+//        user.setId(1L);
+        response = new ResponseEntity<Object>(user, HttpStatus.OK);
+        return response;
+    }
+    
+}
